c9d9c5fe7f5c882cd3077dd9819c62f6aeedb352
Bilal-EZ-ZAIM <bilalzaim499@gmail.com>
2024-05-04 12:56:05 +0100

    challenge 4  en javaScript

---
+++ challenge4.js
@@ -0,0 +1,12 @@
+// Défi 4 : Répétition de chaîne
+
+// Description
+
+// Écrivez une fonction appelée repeatStr qui répète la chaîne donnée exactement n fois
+
+function repeatStr(nombre, texte) {
+    return texte.repeat(nombre);
+}
+
+console.log(repeatStr(6, "I"));
+console.log(repeatStr(5, "Hello"));
